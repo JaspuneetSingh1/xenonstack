ebd70751654d8b4a5e7082f67d0e231b9583b4c0
JaspuneetSingh1 <60125607+JaspuneetSingh1@users.noreply.github.com>
2021-04-02 03:31:19 +0530

    Update address.js

--- address.js
+++ address.js
@@ -5,3 +5,19 @@ function addressmaker(city,state) {
 }
 
 addressmaker('Amritsar', 'Punjab');
+
+
+
+//also it can be done like this with using the assignment operator
+
+
+function addressMaker(address) {
+  const{city, state} = address;
+  
+  const newAddress = {
+    city, state };
+  
+  console.log(`${newAddress.city}, ${newAddress.state}`)
+}
+
+addressmaker({city: 'Amritsar', state: 'Punjab'});
