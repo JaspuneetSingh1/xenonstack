5c332560f5ccc70b210f6b7157ab4c64882169dc
JaspuneetSingh1 <60125607+JaspuneetSingh1@users.noreply.github.com>
2021-04-08 00:46:59 +0530

    using props

---
+++ React/dom.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+ 
+class Hello extends React.Component{
+  render () {
+   return <h1>Hello, {this.props.name}!</h1>;
+  }
+}
+ 
+Hello.defaultProps = {
+  name: "Jaspuneet Singh",
+};
+ 
+ReactDOM.render(<Hello />, document.getElementById('root'));
+ 
+export default Hello;
